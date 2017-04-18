b15f5c1cde2236fd440609a02b06d1de3a669427
ptoivola <ptoivola@users.noreply.github.com>
2017-04-19 00:01:41 +0300

    Update terco.sh

--- terco.sh
+++ terco.sh
@@ -13,10 +13,9 @@ profile_name=tomato #Not meaningful at the moment.
 default_grid_size=32 #supported: 4,8,16,32. Maximum amount of terminals per window.
 line_number=1
 echo -e '\033[0;31m'WARNING!'\033[0m'
-echo -e '\033[0;31m'We will write to ${work_dir}'\033[0m'
-echo -e '\033[0;31m'and ${original_config}'\033[0m'
-echo -e '\033[0;31m'This will destroy your existing terminator configuration in'\033[0;32m' ${original_config}'\033[0m'
-echo -e '\033[0;31m'Terminate script with Ctrl + c '\033[0m'
+echo -e '\033[0;31m'We will write to ${work_dir}.'\033[0m'
+echo -e '\033[0;31m'This will destroy your existing terminator configuration in'\033[0;32m' ${original_config}.'\033[0m'
+echo -e '\033[0;31m'Terminate now with Ctrl + c. '\033[0m'
 read nullers
 ask_user_consent "We will use ${cmd_list}"
 if [ ! -d "${work_dir}" ]; then
